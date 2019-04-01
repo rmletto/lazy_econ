6dbd382ec87c94df3093c5b446982579efb51c01
rmletto <rmletto@mun.ca>
2019-04-01 20:38:58 -0230

    Things

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # lazy_econ
+
+Hi.
+I don't like doing economics. It's repetitive and boring. So, instead of putting compound interest factors into my calculator a million times, I made functions for them instead. Then I put them on GitHub because I wanted them on my desktop PC. Then I thought about putting this module on pip before realizing that maybe I should get back to studying (I'll do it next time I want to put off calculating an IRR).
---
+++ functions.py
@@ -0,0 +1,68 @@
+"""Compound interest factors for lazy people like me"""
+
+
+def fpin(i, n):
+    """The convert present worth to future worth one
+
+    Args:
+        i: interest rate in decimal
+        n: number of periods or whatever
+
+    Returns:
+        The convert present worth to future worth factor thing
+    """
+    return (1+i)**n
+
+
+def pain(i, n):
+    """The convert annuity to present worth one
+
+    Args:
+        i: interest rate in decimal
+        n: number of periods or something
+
+    Returns:
+        pain
+    """
+    return (fpin(i,n)-1)/(fpin(i,n)*i)
+
+
+def apin(i, n):
+    """The opposite of pain
+
+    Args:
+        i: decimal interest rate
+        n: # of periods
+
+    Returns:
+        inverse pain
+    """
+    return 1/pain(i,n)
+
+
+def pfin(i, n):
+    """Convert future worth to present worth
+
+    Args:
+        i: interest rate in decimal
+        n: # of periods
+
+    Returns:
+        Conversion factor for changing future worth to present worth
+    """
+    return 1/fpin(i,n)
+
+
+def cap_recov(p, s, i, n):
+    """Capital recovery factor
+
+    Args:
+        p: The present worth of the thing
+        s: The thing's salvage value after n years
+        i: decimal interest rate
+        n: # of compounding periods
+
+    Returns:
+        Whatever the capital recovery factor thing means
+    """
+    return (p-s)*apin(i,n) + s*i
